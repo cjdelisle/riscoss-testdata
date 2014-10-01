aa08e8799e4fdb7beaa9cfd0c5dd8ae65b3eacd4
Caleb James DeLisle <cjd@cjdns.fr>
2014-10-01 15:29:12 +0200

    initial checkin

---
+++ src/xwiki/this.js
@@ -0,0 +1,6 @@
+Package(function (pkg, XWiki) {
+    pkg.setXarName('riscoss-test-data-0.20-SNAPSHOT.xar');
+    pkg.setName("RISCOSS - Test Data");
+    pkg.setDescription("");
+    pkg.setExtensionId("eu.riscoss:riscoss-testdata");
+});
